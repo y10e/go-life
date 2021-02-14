85d74c5d3dd6d3676da9a19261fef2af5940e5ec
y10e <y10exxx@gmail.com>
2021-02-14 23:43:01 +0900

    中途はんぱ

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/y10e/go-life
 
 go 1.13
 
-require github.com/gdamore/tcell v1.4.0
+require (
+	github.com/gdamore/tcell v1.4.0
+	github.com/mattn/go-runewidth v0.0.7
+)
--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/gdamore/tcell"
 )
 
 func main() {
 
-	const maxGen int = 1000
-	const x, y = 50, 50
+	const maxGen int = 10
+	const x, y = 30, 30
 	
 	NewWorld(x, y)
+	defer world.Stop()
+
+	quit := make(chan struct{})
+	go func(){
+		ev := screen.PollEvent()
+		switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEnter {
+					fmt.Println("enter")
+					close(quit)
+					break
+				}
+		}
+	}()
+
+	<-quit
+
 	for i := 0; i < maxGen; i++ {
 		time.Sleep(time.Millisecond * 100)
 		world.Update()
+		
 	}
-	world.Stop()
+	//<-quit
+	//world.Stop()
 
 }
